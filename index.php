b0b38797bae1175af5b20bba3789618b0cc1a0db
MosesHimself <hmkwp5@mail.missouri.edu>
2018-03-30 13:15:08 -0400

    refactored code

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 function makeHTML( $number )  {
 	echo "<div id='sliderDiv" . $number . "' class='mid part'>";
-	echo "<button type='button' id='iButton" . $number . "' onclick='decrement(" . $number . ")' class='font change'>Decrement</button>";
+	echo "<button type='button' id='iButton" . $number . "' onclick='decrement(" . $number . ")' class='font2 change'>Decrement</button>";
 	echo "<input type='range' id='slider" . $number . "' value='255' min='0' max='255' oninput='slide(" . $number . ")' onchange='send()'/>";
-	echo "<button type='button' id='dButton" . $number . "' onclick='increment(" . $number . ")' class='font change'>Increment</button></div>";
+	echo "<button type='button' id='dButton" . $number . "' onclick='increment(" . $number . ")' class='font2 change'>Increment</button></div>";
 	echo "<div id='numDiv" . $number . "' class='part mid'>";
-	echo "<input type='number' required id='num" . $number . "' class='font' value='255' min='0' max='255' onchange='update(" . $number . ")' oninput='update(" . $number . ")'></div>";
+	echo "<input type='number' required id='num" . $number . "' class='font2' value='255' min='0' max='255' onchange='update(" . $number . ")' oninput='update(" . $number . ")'></div>";
 }
 
 ?>
@@ -17,22 +17,21 @@ function makeHTML( $number )  {
     <meta charset="UTF-8">
     <title>Color Picker App</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Limelight" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Limelight" rel="stylesheet">
     <script src="scripts.js">
     </script>
   </head>
   <body id="body" onload="readHex()">
   	<div id="wrapper">
   		<div id="header" class="mid">
-  			<h1 id="title" class='font'>RGB Color Picker</h1>
-  			<h2 id="description" class='font'>Created by HG King</h2>
+  			<h1 id="title" class='font2'>C H R O M A T A</h1>
+  			<h2 id="description" class='font2'>Created by HG King</h2>
   		</div>
+			<br>
   		<div id="datContainer" class="mid">
-				<form id="myForm" method="get" action="">
-  					<input type="text" id="hex" class='font' required pattern="^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" value="" onchange="hexUpdate()">
-  					<input type="hidden" id="hid" name="hex">
-				</form>
-  			<input type="text" id="rgb" class='font' value="rgb(255,255,255)" onchange="">
+  			<input type="text" id="rgb" class='font2' required pattern="^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" value="" onchange="hexUpdate()">
+  			<input type="text" id="hex" class='font2' value="rgb(255,255,255)" onchange="">
+				<input type="text" id="hsl" class='font2' value="hsl(0,0,0)" onchange="">
   		</div>
   		<br>
   		<div id="topContainer" class="mid color">
@@ -51,13 +50,12 @@ function makeHTML( $number )  {
   			<input type='range' id='sliderR' value='500' min='0' max='1000' oninput='rainbowSlider()' onchange='send()'/>
   		</div>
   		<div id="butContainer" class="mid">
-			<button type="button" id='blueButton' onclick="window.location.href='/color_picker/picker.php?hex=0000ff'" class='font'>Blue</button>
-	  		<button type="button" id='bButton' onclick="window.location.href='/color_picker/picker.php?hex=000000'" class='font'>Black</button>
-			<button type="button" id='rButton'  onclick="random()" class='font change'>Random</button>
-			<button type="button" id='wButton' onclick="window.location.href='/color_picker/picker.php?hex=ffffff'" class='font'>White</button>
-			<button type="button" id='redButton' onclick="window.location.href='/color_picker/picker.php?hex=ff0000'" class='font'>Red</button>
+			<button type="button" id='blueButton' onclick="setColors(0, 0, 255)" class='font2'>Blue</button>
+	  		<button type="button" id='bButton' onclick="setColors(0, 0, 0)" class='font2'>Black</button>
+			<button type="button" id='rButton'  onclick="random()" class='font2 change'>Random</button>
+			<button type="button" id='wButton' onclick="setColors(255, 255, 255)" class='font2'>White</button>
+			<button type="button" id='redButton' onclick="setColors(255, 0, 0)" class='font2'>Red</button>
 		</div>
-  		
   	</div>
   </body>
 </html>
--- scripts.js
+++ scripts.js
@@ -1,65 +1,92 @@
+//runs when page is loaded
 function readHex()  {
-	var hex = getParameterByName("hex");
-	var l = hex.length;
-	if(l < 3 || l > 6)  {
-		random();
-		return;
+	if(window.location.hash)  {
+
+		var hex = window.location.hash;
+		//alert(hex);
+		var len = hex.length;
+		if(len == 7)  {
+			setColors(hexToR(hex), hexToG(hex), hexToB(hex));
+		}
+		else random();
 	}
-	setColors(hexToR(hex), hexToG(hex), hexToB(hex));
-	formColorString();
+	else random();
 }
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function formColorString()  {
-	var x = document.getElementById("num0").value;
-	var y = document.getElementById("num1").value;
-	var z = document.getElementById("num2").value;
-	
-	var string = "rgb(" + x + "," + y + "," + z + ")";
-	document.getElementById('rgb').value = string;
-	
-	var hex = rgbToHex(x, y, z);
-	document.getElementById('hex').value = "#" + hex;
-	document.getElementById('hid').value = hex;
-	document.body.style.backgroundColor = string;
-	
-	document.getElementById("title").style.textShadow = "2px 2px "+ string;
-	
-	var c = document.getElementsByClassName("change");
-	var i;
-	for (i = 0; i < c.length; i++) {
-		c[i].style.backgroundColor = string;
+//runs when the hex field is changed
+function hexUpdate()  {
+	var hex = document.getElementById('hex').value;
+	if(hex[0] == "#" && hex.length == 7)  {
+		window.location.hash = hex;
+		readHex();
 	}
-	
-	document.getElementById("description").style.color = string;
+	else random();
 }
 
+//sets colors and hash. Definitive method to change the page
 function send()  {
 	formColorString();
-	document.getElementById("myForm").submit();
+	window.location.hash = document.getElementById('hex').value;
 }
 
+//update a given color
 function update(n)  {
-	
-	if(document.getElementById("num" + n).value == undefined || document.getElementById("num" + n).value < 0)  {
+	if(document.getElementById("num" + n).value == '' || document.getElementById("num" + n).value == undefined || document.getElementById("num" + n).value < 0)  {
 		document.getElementById("num" + n).value = 0;
 		//alert("shit was empty");
 	}
 	else if(document.getElementById("num" + n).value > 255)  {
 		document.getElementById("num" + n).value = 255;
 	}
-	
+
 	document.getElementById("slider" + n).value = document.getElementById("num" + n).value;
 	send();
+}
+
+//this takes the values from the 3 numbers and updates the page colors
+function formColorString()  {
+	var r = document.getElementById("num0").value;
+	var g = document.getElementById("num1").value;
+	var b = document.getElementById("num2").value;
+
+	var string = "rgb(" + r + "," + g + "," + b + ")";
+	document.getElementById('rgb').value = string;
+
+	var hex = rgbToHex(r, g, b);
+	document.getElementById('hex').value = "#" + hex;
+	document.body.style.backgroundColor = string;
+
+
+	var c = document.getElementsByClassName("change");
+	var i;
+	for (i = 0; i < c.length; i++) {
+		c[i].style.backgroundColor = string;
+	}
+
+
+	var hsl = rgbToHSL(r, g, b);
+	document.getElementById('hsl').value = hsl;
+	//window.location.hash = hex;
+}
+
+//explicitly set each number to a given value
+function setColors(r, g, b)  {
+	setR(r);
+	setG(g);
+	setB(b);
+	send();
+}
+
+function setR(r)  {
+	document.getElementById("slider0").value=document.getElementById("num0").value = r;
+}
+
+function setG(g)  {
+	document.getElementById("slider1").value=document.getElementById("num1").value = g;
+}
+
+function setB(b)  {
+	document.getElementById("slider2").value=document.getElementById("num2").value = b;
 }
 
 function increment(n)  {
@@ -77,86 +104,165 @@ function slide(n)  {
 	formColorString();
 }
 
+function random()  {
+	var r = Math.floor(Math.random() * 255);
+	var g = Math.floor(Math.random() * 255);
+	var b = Math.floor(Math.random() * 255);
 
-function setColors(x, y, z)  {
-	document.getElementById("num0").value = x;
-	document.getElementById("num1").value = y;
-	document.getElementById("num2").value = z;
-	
-	document.getElementById("slider0").value = document.getElementById("num0").value;
-	document.getElementById("slider1").value = document.getElementById("num1").value;
-	document.getElementById("slider2").value = document.getElementById("num2").value;
-	
+	setColors(r, g, b);
 }
 
 function rainbowSlider()  {
-
 	var i = (document.getElementById("sliderR").value / 159.1);
-
-	var x = Math.round(127 * Math.cos(i  + (3.14159 * 2))) + 128;
-    var y = Math.round(127 * Math.sin(i + (3.14159 * 2))) + 128;
-    var z = Math.round(127 * Math.cos(i + 3.14159)) + 128;
-    
-    setColors(x, y, z);
+	setR(Math.round(127 * Math.cos(i  + (3.14159 * 2))) + 128);
+  setG(Math.round(127 * Math.sin(i + (3.14159 * 2))) + 128);
+  setB(Math.round(127 * Math.cos(i + 3.14159)) + 128);
 	formColorString();
 }
 
-function random()  {
-	var x = Math.floor(Math.random() * 255);
-	var y = Math.floor(Math.random() * 255);
-	var z = Math.floor(Math.random() * 255);
-	
-	setColors(x, y, z);
-	send();
-}
 
-function rgbUpdate()  {
-
-}
-
-function hexUpdate()  {
-	var hex = document.getElementById('hex').value;
-	if(hex[0] == "#" && hex.length == 7)  {
-		window.location.href='/color_picker/picker.php?hex=' + hex.substring(1,7);
-	}
-}
-
+//this funciton takes a number and returns the hex value in a string
 function componentToHex(c) {
 
 	//keeps the value at 2 digits
 	if(c < 16)  return "0" + toH(c);
-	
+
 	var string = "";
 	var remainder = 0;
 	while (c > 0)  {
 		remainder = c % 16;
 		remainder = toH(remainder);
-		
+
 		string = remainder + string;
 		c = c / 16;
 		c = Math.floor(c);
 	}
-	
+
 	return string;
 }
 
+//this turns each digit into the associated hex value
 function toH(c)  {
 
-	if(c == 10) return 'a';
-	else if(c == 11) return "b";
-	else if(c == 12) return "c";
-	else if(c == 13) return "d";
-	else if(c == 14) return "e";
-	else if(c == 15) return "f";
+	if(c == 10) return 'A';
+	else if(c == 11) return "B";
+	else if(c == 12) return "C";
+	else if(c == 13) return "D";
+	else if(c == 14) return "E";
+	else if(c == 15) return "F";
 
 	return c.toString();
 
 }
 
+function rgbToHSL(r, g, b)  {
+	var normR = r / 255;
+	var normG = g / 255;
+	var normB = b / 255;
+	var string = "rgb(" + r + "," + g + "," + b + ")";
+	var max = Math.max(normR, normG, normB);
+	var min = Math.min(normR, normG, normB);
+
+	var chroma = max - min;
+
+	var lightness = (max + min) / 2;
+
+	var saturation;
+
+	if(chroma == 0)  {
+		saturation = 0;
+	}
+	else saturation = chroma / (1 - (Math.abs(lightness * 2 - 1)));
+
+	var hue;
+
+	if(chroma == 0)  {
+		hue = 0;
+	}
+	else if(max == normR)  {
+		hue = 60 * (((normG - normB) / chroma) % 6);
+	}
+	else if(max == normG)  {
+		hue = 60 * (((normB - normR) / chroma) + 2);
+	}
+	else if(max == normB)  {
+		hue = 60 * (((normR - normG) / chroma) + 4);
+	}
+
+	saturation = Math.round(saturation * 100);
+	lightness = Math.round(lightness * 100);
+	hue = Math.round(hue);
+	if(hue > 360)  {
+		hue = 360 - (hue - 360);
+	}
+	if(hue < 0)  {
+		hue = 360 + hue;
+	}
+
+	if(lightness < 10) var string = "hsl("+ hue +", "+ (saturation + 10) +"%, 10%)";		
+	else var string = "hsl("+ hue +", "+ saturation +"%, "+ lightness +"%)";
+
+	document.getElementById("title").style.textShadow = "2px 2px "+ string;
+	document.getElementById("description").style.color = string;
+
+	return "hsl("+ hue +"°, "+ saturation +"%, "+ lightness +"%)";
+}
+
+function hslToRGB(hue, saturation, lightness)  {
+	alert(lightness);
+	alert(hue);
+	var chroma = (1 - Math.abs((2 * lightness) - 1)) * saturation;
+
+	var x = chroma * (1 - Math.abs(((hue / 60) % 2) - 1));
+
+	var m = lightness - (chroma / 2);
+
+	var red, green, blue;
+
+	if(hue >= 0 && hue < 60)  {
+		red = chroma;
+		green = x;
+		blue = 0;
+	}
+	else if(hue >= 60 && hue < 120)  {
+		red = x;
+		green = chroma;
+		blue = 0;
+	}
+	else if(hue >= 120 && hue < 180)  {
+		red = 0;
+		green = chroma;
+		blue = x;
+	}
+	else if(hue >= 180 && hue < 240)  {
+		red = 0;
+		blue = x;
+		green = chroma;
+	}
+	else if(hue >= 240 && hue < 300)  {
+		red = x;
+		green = 0;
+		blue = chroma;
+	}
+	else if(hue >= 300 && hue < 360)  {
+		red = chroma;
+		green = 0;
+		blue = x;
+	}
+
+	red = (red + m) * 255;
+	green = (green + m) * 255;
+	blue = (blue + m) * 255;
+
+	return "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+//utility funciton to break up rgb values
 function rgbToHex(r, g, b) {
 	return componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
+//following functions break a hex value up into the resulting rgb values
 function hexToR(h) {
 	var digits = cutHex(h);
 	var r = parseInt(digits.substring(0, 2), 16);
